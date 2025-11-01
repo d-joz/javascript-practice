1dec1e38e6d70863b3a9447042a9983e118ede81
Adithyan <153833893+d-joz@users.noreply.github.com>
2025-11-01 18:49:20 +0530

    .....

--- my-kind of-things/wordle.js
+++ my-kind of-things/wordle.js
@@ -1,61 +1,172 @@
 // not completed yet
-const GREY = 240;
-const GREEN = 34;
-const YELLOW = 220;
+const GREY = "240";
+const GREEN = "034";
+const YELLOW = "220";
+const WHITE = "251";
 
-function color(text, code, bold = '') {
+const DISPLAY_LENGTH = 156;
+
+function color(text, code = WHITE, bold = "") {
   return "\x1B[" + bold + ";38;5;" + code + "m" + text + "\x1B[0m";
 }
 
-function displayWord(word) {
+function displayWord(word, length) {
   const formatted = [];
 
   for (let index = 0; index < 3; index++) {
-    formatted.push(extractLine(word, index));
+    formatted.push(padCentre(extractLine(word, index), length));
   }
 
-  return formatted.join('\n')
+  return formatted.join("\n");
 }
 
 function extractLine(word, lineNo) {
-  let line = ''
+  let line = "";
   for (let index = 0; index < word.length; index++) {
-    line += word[index][lineNo]
+    line += word[index][lineNo];
   }
 
-  return line
+  return line;
 }
 
-function generateLetter(text, style = GREY) {
-  const topLeftCorner = color('┏', style);
-  const topRightCorner = color('┓', style);
-  const bottomLeftCorner = color('┗', style);
-  const bottomRightCorner = color('┛', style);
-  const vertical = color('┃', style);
-  const horizontal = color('━', style);
+function generateLetter(text, style) {
+  const topLeftCorner = color("┏", style);
+  const topRightCorner = color("┓", style);
+  const bottomLeftCorner = color("┗", style);
+  const bottomRightCorner = color("┛", style);
+  const vertical = color("┃", style);
+  const horizontal = color("━", style);
 
   const letter = color(text, style, 1);
   const letterTemplate = [
     [topLeftCorner + horizontal.repeat(3) + topRightCorner],
     [vertical + ` ${letter} ` + vertical],
-    [bottomLeftCorner + horizontal.repeat(3) + bottomRightCorner]
+    [bottomLeftCorner + horizontal.repeat(3) + bottomRightCorner],
   ];
+
   return letterTemplate;
 }
 
-function main() {
-
-  const word = 'ADHI';
+function formatWord(word) {
   const arr = [];
 
   for (let index = 0; index < word.length; index++) {
-    arr.push(generateLetter(word[index], GREEN));
+    arr.push(generateLetter(word[index][0], word[index][1]));
   }
 
-  console.log(displayWord(arr));
+  return arr;
+}
 
+function displayBoard(board) {
+  const arr = [];
   
+  for (let index = 0; index < board.length; index++) {
+    arr.push(displayWord(formatWord(board[index]), board[index].length));
+  }
+  console.log(arr.join("\n"));
+}
 
+function padCentre(string, length) {
+  const leadingSpaces = Math.floor((DISPLAY_LENGTH - length * 5) / 2);
+  const padLength = leadingSpaces + string.length;
+
+  return string.padStart(padLength);
+}
+
+function colorMap(word) {
+  const mapped = [];
+
+  for (let index = 0; index < word.length; index++) {
+    const element = [word[index], WHITE];
+    mapped.push(element);
+  }
+
+  return mapped;
+}
+
+function createGuessArea(wordLength, chances) {
+  const guessArea = [];
+
+  for (let index = 0; index < chances; index++) {
+    guessArea.push(colorMap(" ".repeat(wordLength)));
+  }
+  return guessArea;
+}
+
+function findMatches(guess, secretWord, board, chances) {
+  const guessArea = board;
+  for (let index = 0; index < guess.length; index++) {
+    ((guessArea[chances][index] =
+      guess[index] === secretWord[index]
+        ? [guess[index], GREEN]
+        : guess[index]),
+    GREY),
+      GREY;
+  }
+  return guessArea;
+}
+
+function updateKeyboard(guess, secretWord, keyboard) {
+  return keyboard
+}
+
+function main() {
+  const secretWord = "ADHI";
+  let chances = 5;
+  let guessArea = createGuessArea(secretWord.length, chances);
+
+  let keyboard = [
+    [
+      ["Q", WHITE],
+      ["W", WHITE],
+      ["E", WHITE],
+      ["R", WHITE],
+      ["T", WHITE],
+      ["Y", WHITE],
+      ["U", WHITE],
+      ["I", WHITE],
+      ["O", WHITE],
+      ["P", WHITE],
+    ],
+    [
+      ["A", WHITE],
+      ["S", WHITE],
+      ["D", WHITE],
+      ["F", WHITE],
+      ["G", WHITE],
+      ["H", WHITE],
+      ["J", WHITE],
+      ["K", WHITE],
+      ["L", WHITE],
+    ],
+    [
+      ["Z", WHITE],
+      ["X", WHITE],
+      ["C", WHITE],
+      ["V", WHITE],
+      ["B", WHITE],
+      ["N", WHITE],
+      ["M", WHITE],
+    ],
+  ];
+  console.clear();
+  displayBoard(guessArea);
+  console.log("\n");
+  displayBoard(keyboard);
+
+  for (let index = 0; index < chances; index++) {
+    const guess = prompt("enter your guess :");
+
+    guessArea = findMatches(guess, secretWord, guessArea, index);
+    keyboard = updateKeyboard(guess, secretWord, keyboard);
+
+    console.clear();
+    // console.log(guessArea, 'in loop')
+    // console.log(keyboard, '................in loop')
+    displayBoard(guessArea);
+    console.log("\n");
+    displayBoard(keyboard);
+  }
 }
 
 main();
